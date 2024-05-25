22d06ae8303977a34b621e87f83ddfbb0986f4a8
Ramling <vikrant@mobigic.com>
2024-05-25 11:46:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,33 +3,19 @@ import React, { useState, useEffect } from "react";
 import TaskList from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 import TaskFilter from "./components/TaskFilter";
-import Auth from "./components/Auth";
-import { auth } from "./firebase";
 
 const App = () => {
-  const [user, setUser] = useState(null);
   const [tasks, setTasks] = useState([]);
   const [filter, setFilter] = useState("all");
 
   useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    if (storedUser) {
-      setUser(storedUser);
-    }
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    setTasks(storedTasks);
   }, []);
 
   useEffect(() => {
-    if (user) {
-      const storedTasks = JSON.parse(localStorage.getItem(`tasks-${user.uid}`)) || [];
-      setTasks(storedTasks);
-    }
-  }, [user]);
-
-  useEffect(() => {
-    if (user) {
-      localStorage.setItem(`tasks-${user.uid}`, JSON.stringify(tasks));
-    }
-  }, [tasks, user]);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const addTask = (task) => {
     setTasks([...tasks, task]);
@@ -58,36 +44,19 @@ const App = () => {
     if (filter === "active") return !task.completed;
     return true;
   });
-console.log(user);
+
   return (
     <div className="app">
-      <h1>Task Manager </h1>
-      <h1>{user?.displayName?user?.displayName:" "}</h1>
-      
-      {!user ? (
-        <Auth setUser={setUser} />
-      ) : (
-        <>
-          <TaskForm addTask={addTask} />
-          <TaskFilter filter={filter} setFilter={setFilter} />
-          <TaskList
-            setTasks={setTasks}
-            tasks={filteredTasks}
-            editTask={editTask}
-            deleteTask={deleteTask}
-            toggleComplete={toggleComplete}
-          />
-          <button  style={{marginTop:"10px",backgroundColor:"brown",color:"white"}}
-            onClick={() => {
-              auth.signOut();
-              setUser(null);
-              localStorage.removeItem("user");
-            }}
-          >
-            Sign Out
-          </button>
-        </>
-      )}
+      <h1>Task Manager</h1>
+      <TaskForm addTask={addTask} />
+      <TaskFilter filter={filter} setFilter={setFilter} />
+      <TaskList
+      setTasks={setTasks}
+        tasks={filteredTasks}
+        editTask={editTask}
+        deleteTask={deleteTask}
+        toggleComplete={toggleComplete}
+      />
     </div>
   );
 };
